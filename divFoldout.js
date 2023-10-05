1c51c55ea70afe4529b4204162da1a41565b0505
TheisKidi <theis.k@hotmail.com>
2023-10-05 09:54:41 +0200

    Nye updates

--- divFoldout.js
+++ divFoldout.js
@@ -1,8 +1,11 @@
-function toggleDetails(weekId) {
+function toggleDetails(weekId, thinId) {
   const weekDetails = document.getElementById(weekId);
+  const thinP = document.getElementById(thinId)
   if (weekDetails.style.display === "block") {
       weekDetails.style.display = "none";
+      thinP.style.display = "none";
   } else {
       weekDetails.style.display = "block";
+      thinP.style.display = "block";
   }
 }
